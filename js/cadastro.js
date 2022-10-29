401ba78d5268cc5436894cdc5f85c623a4be79df
Renan Cesar <cesar.renan@hotmail.com>
2022-10-28 21:29:31 -0300

    Funcionalidade de limpar input após envio

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,8 +1,8 @@
-const url = 'http://localhost:3000'
+const url = 'http://localhost:3000';
 
-const urlUsuarios = `${url}/usuarios`
+const urlUsuarios = `${url}/usuarios`;
 
-const urlVagas = `${url}/vagas`
+const urlVagas = `${url}/vagas`;
 
 class Usuario {
   id
@@ -14,28 +14,28 @@ class Usuario {
   candidaturas = []
 
   constructor(categoria, nomeCompleto, dataDeNascimento, email, senha) {
-    this.categoria = categoria
-    this.nomeCompleto = nomeCompleto
-    this.dataDeNascimento = dataDeNascimento
-    this.email = email
-    this.senha = senha
+    this.categoria = categoria;
+    this.nomeCompleto = nomeCompleto;
+    this.dataDeNascimento = dataDeNascimento;
+    this.email = email;
+    this.senha = senha;
   }
 }
 
 const enviar = event => {
-  event.preventDefault()
+  event.preventDefault();
 
-  cadastrarUsuario()
+  cadastrarUsuario();
 }
 
 const cadastrarUsuario = async () => {
-  const categoriaDeUsuario = document.getElementById('categoria-usuario')
-  const nomeCompleto = document.getElementById('nome-usuario')
-  const dataDeNascimento = document.getElementById('nascimento')
-  const email = document.getElementById('email')
-  const senha = document.getElementById('senha')
+  const categoriaDeUsuario = document.getElementById('categoria-usuario');
+  const nomeCompleto = document.getElementById('nome-usuario');
+  const dataDeNascimento = document.getElementById('nascimento');
+  const email = document.getElementById('email');
+  const senha = document.getElementById('senha');
 
-  const dataFormatada = dataDeNascimento.value.split('-')
+  const dataFormatada = dataDeNascimento.value.split('-');
 
   const novoUsuario = new Usuario(
     categoriaDeUsuario.value,
@@ -43,20 +43,29 @@ const cadastrarUsuario = async () => {
     new Date(dataFormatada[0], dataFormatada[1], dataFormatada[2]),
     email.value,
     senha.value
-  )
-
+  );
+    
   try {
     await axios.post(`http://localhost:3000/usuarios`, novoUsuario)
     alert('Parabéns, você foi cadastrado!')
   } catch (error) {
     alert('Algo deu errado')
-  }
+  } 
+
+  limparInput();
 }
+
+let limparInput = ()=> {
+  document.getElementById('nome-usuario').value = '';
+  document.getElementById('email').value = '';
+  document.getElementById('senha').value = '';
+}
+
 
 let logar = event => {
-  event.preventDefault()
+  event.preventDefault();
 
-  verificarCategoriaUsuario()
+  verificarCategoriaUsuario();
 }
 
-let verificarCategoriaUsuario = () => {}
+let verificarCategoriaUsuario = () => {};
